081254428606e3ac5b9b9c0a3368376696bf9874
Baleksas <alexasbag17@gmail.com>
2022-11-03 15:57:06 +0000

    feat: tutorial fixing errors. Works as expected currently

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,28 +1,26 @@
-// import PocketBase from 'pocketbase';
-import Link from 'next/link';
-import styles from './Notes.module.css';
-import CreateNote from './Create';
+import PocketBase from "pocketbase";
+import Link from "next/link";
+import styles from "./Notes.module.css";
+import CreateNote from "./CreateNote";
 
-// export const dynamic = 'auto',
-//   dynamicParams = true,
-//   revalidate = 0,
-//   fetchCache = 'auto',
-//   runtime = 'nodejs',
-//   preferredRegion = 'auto'
-
+export const dynamic = "auto",
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = "auto",
+  runtime = "nodejs",
+  preferredRegion = "auto";
 
 async function getNotes() {
-  // const db = new PocketBase('http://127.0.0.1:8090');
-  // const result = await db.records.getList('notes');
-  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
-  const data = await res.json();
+  const db = new PocketBase("http://127.0.0.1:8090");
+  const data = await db.records.getList("notes");
+
   return data?.items as any[];
 }
 
 export default async function NotesPage() {
   const notes = await getNotes();
 
-  return(
+  return (
     <div>
       <h1>Notes</h1>
       <div className={styles.grid}>
